6778b389b1395678e2ef98c8c508def98010d56f
Nasif Mustufa Khan <nasifmustufakhan7220@gmail.com>
2026-01-15 21:27:17 +0600

    06_coments,printFunc& Escape Sequence file added

---
+++ 06_coments,printFunc& Escape Sequences/printFuncDetails.py
@@ -0,0 +1,9 @@
+# print("Hello world", "How are you? ", 5)
+
+# print("Hello world", "How are you?", 5, sep = ",")
+# Output : Hello world,How are you?,5
+
+print("Hello world", end = "!")
+print("Nasif")
+
+# Output : Hello world!Nasif
