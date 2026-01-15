c7de1e4bf90e7f7282a804614686d8d5802d198e
Nasif Mustufa Khan <nasifmustufakhan7220@gmail.com>
2026-01-15 20:53:42 +0600

    Another python file added

---
+++ 05_inputFunc/main.py
@@ -0,0 +1,10 @@
+#a = input("Enter a number: ")
+#b = input("Enter another number: ")
+
+#sum_num = int(a) + int(b)
+
+#print("You entered:",sum_num, "\n",type(sum_num))
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello", name + ", you are", age, "years old.")
