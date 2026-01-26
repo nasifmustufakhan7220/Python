f016e8f6dfc976c5576000a7e7a1ecdafaa0a132
Nasif Mustufa Khan <nasifmustufakhan7220@gmail.com>
2026-01-26 19:41:45 +0600

    new file added

---
+++ 09_forLoop.py/main.py
@@ -0,0 +1,9 @@
+for i in range(5) :
+    print(i)
+
+# for i in range(4,10) : #10-4 = 6. It means that, for loop will print from 4 to unless count of 6.
+#     print(i)
+
+
+# for i in range(1,11) :
+#     print("5 X", i, "=", 5*i)
